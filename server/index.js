36b3eb78c0f9daa55e9a6188825c19278924518b
ardydj <ardydj07@gmail.com>
2020-10-31 16:37:04 -0700

    Create React Index.js And Express Server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Worldddd');
+
+})
+
+app.listen(port, () => {
+  console.log(`server listening at port ${port}`)
+})
