a1b926b7ec116b38ccf7253b308ee896b318ede0
ardydj <ardydj07@gmail.com>
2020-11-03 15:49:51 -0800

    Implement Express Server with Public Directory

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello Worldddd');
+app.use(express.static(path.join(__dirname, '/../client/dist')))
 
-})
+app.use(express.json());
 
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
 app.listen(port, () => {
   console.log(`server listening at port ${port}`)
 })
