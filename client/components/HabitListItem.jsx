20860d8f86476b6186232f1ac91bc7b23b9d9e16
ardydj <ardydj07@gmail.com>
2020-11-06 11:08:43 -0800

    Implement Font Styling For Streak

--- client/components/HabitListItem.jsx
+++ client/components/HabitListItem.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const HabitListItem = (props) => {
   return (
     <div className="individual-habit">
-        <div>
+        <div className="habit-title">
         {props.habitItem.title}
         </div>
         <button className="habit-button" onClick={() => {props.handleCheckMark(props.habitIndex)}}>√</button>
         <button className="habit-button" onClick={() => {props.handleCrossMark(props.habitIndex)}}>X</button>
-        <div>Streak: {props.habitItem.streak} days</div>
+        <div className="habit-streak">Streak: {props.habitItem.streak}</div>
     </div>
   );
 }
