b0b3737e2a031aba281b3d176ba8477191b95f82
ardydj <ardydj07@gmail.com>
2020-11-06 08:32:51 -0800

    Implement Padding For Success Rate

--- client/components/HabitList.jsx
+++ client/components/HabitList.jsx
@@ -4,8 +4,8 @@ import HabitListItem from './HabitListItem.jsx';
 const HabitList = (props) => {
   console.log('This is from habitList: ', props.habits);
   return (
-    <div>
-      <div>Success Rate: {props.successCompute()}%</div>
+    <div id="habit-container">
+      <div id="success-rate">Success Rate: {props.successCompute()}%</div>
       <div>{props.habits.map((habit, index) => <HabitListItem habitItem={habit} key={habit.title} habitIndex={index} handleCheckMark={props.handleCheckMark} handleCrossMark={props.handleCrossMark}/>)}</div>
     </div>
   );
