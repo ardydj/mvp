8bc149c37739c2181a08a384d8797aa285a6868a
ardydj <ardydj07@gmail.com>
2020-11-28 13:01:44 -0500

    Add Compression To Express Server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const port = 3000;
 const path = require('path');
 const cors = require('cors');
 const Habits = require('./sampleData.js');
+const compression = require('compression');
 
 app.use(express.static(path.join(__dirname, '/../client/dist')))
 app.use(cors());
 app.use(express.json());
+app.use(compression());
 
 app.get('/api/habits', (req, res) => {
   res.send(Habits);
